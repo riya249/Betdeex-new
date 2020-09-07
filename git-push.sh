71d2753e2c99037ccd8208f51424c3c43af5961a
mahesh <mahesh@blocklogy.org>
2020-09-07 18:13:03 +0530

    bash file modified

--- git-push.sh
+++ git-push.sh
@@ -1,8 +1,6 @@
-echo "Fetch working directory status: "
-git status
-
-echo "Enter Commit Message: "
-read message
-
-git add . && git commit -m '$message' && git push
+echo 'Commit & Push Operation will be performed using ssh, make sure ssh public key added to github'
+read -p 'Commit Message: ' message
+git add .
+git commit -m "$message"
+git push
 
